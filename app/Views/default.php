13bb691a70f723638926e1c83ff15d376571fe42
Matthew Pennell <github@matthewpennell.com>
2025-02-22 14:00:06 +0000

    Cache busting param for static CSS file

--- app/Views/default.php
+++ app/Views/default.php
@@ -20,7 +20,7 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Serif:ital,opsz,wght@0,8..144,100..900;1,8..144,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=link,open_in_new" />
-        <link rel="stylesheet" href="/css/midair.css">
+        <link rel="stylesheet" href="/css/midair.css?v=<?= filemtime($_SERVER['DOCUMENT_ROOT'] . '/css/midair.css') ?>">
         <script src="https://unpkg.com/htmx.org@2.0.4"></script>
     </head>
     <body hx-boost="true">
