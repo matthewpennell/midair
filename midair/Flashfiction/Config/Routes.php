d76eb3d9b68aa005814a27d6457b435f62b13bdb
Matthew Pennell <github@matthewpennell.com>
2025-10-18 16:50:26 +0100

    Fix bug caused by Tumblr guids being numeric and matching the pagination route

--- midair/Flashfiction/Config/Routes.php
+++ midair/Flashfiction/Config/Routes.php
@@ -2,7 +2,7 @@
 
 $routes->group('flashfiction', ['namespace' => 'Midair\Flashfiction\Controllers'], static function ($routes) {
     $routes->cli('import', 'Import::index');
-    $routes->get('(:num)', 'Display::index/$1');
+    $routes->get('(\d{1,3})', 'Display::index/$1');
     $routes->get('(:any)', 'Display::single/$1');
     $routes->get('/', 'Display::index');
 });
