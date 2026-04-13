e51904ffbb754a591987d725242baefdac97e7bf
Matthew Pennell <github@matthewpennell.com>
2026-04-13 18:58:41 +0100

    Delete static version of site used during development

--- static-site/js/script.js
+++
@@ -1,8 +0,0 @@
-// Main JavaScript file for the website
-// You can add interactivity here as needed
-
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Website loaded successfully!');
-    
-    // Add any JavaScript functionality here
-});
